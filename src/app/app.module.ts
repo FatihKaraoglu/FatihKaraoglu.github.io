889b73116a80a53ecbc8b586c7aa1cbe93d9119d
FatihKaraoglu <karaoglu.fatih16@gmail.com>
2023-05-15 16:10:58 +0200

    NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CubeComponent } from './cube/cube.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CubeComponent
+    NavbarComponent
   ],
   imports: [
     BrowserModule
