13231398469b24305502c20fc552bd36f00f167c
FatihKaraoglu <karaoglu.fatih16@gmail.com>
2023-05-17 12:35:21 +0200

    Routing / Carousel added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,22 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { VerticalcarouselComponent } from './verticalcarousel/verticalcarousel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CarouselComponent
+    CarouselComponent,
+    HomeComponent,
+    VerticalcarouselComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/verticalcarousel/verticalcarousel.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verticalcarousel',
+  templateUrl: './verticalcarousel.component.html',
+  styleUrls: ['./verticalcarousel.component.css']
+})
+export class VerticalcarouselComponent {
+
+}
